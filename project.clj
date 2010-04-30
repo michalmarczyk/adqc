89529ff586b69a12e2a1c92efe3031d10e76eed3
Michał Marczyk <michal.marczyk@gmail.com>
2010-04-30 03:47:49 +0200

    basic repo structure (closes #2)

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject adqc "0.0.1-SNAPSHOT"
+  :description "Adaptive Distributed Query Compiler"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
---
+++ src/adqc/core.clj
@@ -0,0 +1,1 @@
+(ns adqc.core)
