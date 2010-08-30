7783fcb4b37d12b8688b425a26111636473b9d65
Michał Marczyk <michal.marczyk@gmail.com>
2010-08-30 16:15:06 +0200

    bump Clojure version to 1.2.0

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :licence {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[deview/lein-deview "1.0.5"]]
   :deview-server 9000)
